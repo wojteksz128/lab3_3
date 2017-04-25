cf39ac92d522cd65e24e05b616f4b2b58ea5f908
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-25 15:23:25 +0200

    Create empty OrderTest.

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.time;
+
+import org.junit.Before;
+
+/**
+ * Created by Wojciech Szczepaniak on 25.04.2017.
+ */
+public class OrderTest {
+
+    @Before
+    public void setUp() {
+
+    }
+}
