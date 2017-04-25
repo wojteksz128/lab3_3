f94ea38a763b0d4a7a9b6eaeda2263f681dd6430
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-25 15:43:00 +0200

    Test expired order throw exception if you confirm order after submit.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import edu.iis.mto.time.timeprovider.CustomTimeProvider;
 import edu.iis.mto.time.timeprovider.RealTimeProvider;
 import org.junit.Test;
 
@@ -11,6 +12,13 @@ public class OrderTest {
     @Test
     public void notExpiredOrderInConfirmNoThrowException() {
         Order order = new Order(new RealTimeProvider());
+        order.submit();
+        order.confirm();
+    }
+
+    @Test (expected = OrderExpiredException.class)
+    public void expiredOrderInConfirmThrowException() {
+        Order order = new Order(new CustomTimeProvider().addHours(25));
         order.submit();
         order.confirm();
     }
