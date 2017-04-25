66770ef6762754f76415b5fa52924b418d6de536
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-25 14:58:15 +0200

    Implementation of TimeProvider in real time added.

---
+++ src/test/java/edu/iis/mto/time/RealTimeProvider.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.time;
+
+/**
+ * Created by Wojciech Szczepaniak on 25.04.2017.
+ */
+public class RealTimeProvider implements TimeProvider {
+
+    @Override
+    public long currentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+}
