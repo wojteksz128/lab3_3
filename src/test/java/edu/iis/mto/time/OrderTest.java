c1f3560b3c8be1b162773397fff4cf2ad056a60e
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-25 15:35:27 +0200

    Test not expired order not throw exception if you confirm order after submit.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,14 +1,17 @@
 package edu.iis.mto.time;
 
-import org.junit.Before;
+import edu.iis.mto.time.timeprovider.RealTimeProvider;
+import org.junit.Test;
 
 /**
  * Created by Wojciech Szczepaniak on 25.04.2017.
  */
 public class OrderTest {
 
-    @Before
-    public void setUp() {
-
+    @Test
+    public void notExpiredOrderInConfirmNoThrowException() {
+        Order order = new Order(new RealTimeProvider());
+        order.submit();
+        order.confirm();
     }
 }
